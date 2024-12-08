7dee928f2990e12463c46c5732f817886bcaa931
GalPeter24 <galpeti20083@gmail.com>
2024-12-08 15:07:51 +0100

    c_feladat

---
+++ feladat_c.py
@@ -0,0 +1,17 @@
+korongok=[]
+sor= input('Add meg a korong helyének sorát!')
+while sor != "" :
+    sorszam= int(sor)
+    if sorszam <= 8 and sorszam > 0:
+        oszlop= input('Add meg a korong helyének oszlopát!')
+        oszlopszam = int(oszlop)
+        if oszlopszam <= 8 and oszlopszam > 0 :
+            korong = sor + "K" + oszlop
+            if korong in korongok:
+                print("Itt már szerepel korong")
+                korongok.remove(korong)
+            else:korongok.append(korong)
+        else: print('hibás oszlopszám') 
+    else: print('hibás sorszám')
+    sor= input('Add meg a korong helyének sorát!')
+print(korongok)
